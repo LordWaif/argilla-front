0d82685e9d095eb3a4237f74b344893cc3f25566
= <=>
2023-04-25 01:05:49 +0000

    criando graficos dinamicos

--- checkLabel/urls.py
+++ checkLabel/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from .views import home
+from .views import home,graficos
 from django.urls import path
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home,name='index'),
+    path('gen-graph',graficos,name='graficos')
 ]
--- checkLabel/views.py
+++ checkLabel/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from django.template import loader
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
@@ -15,11 +15,13 @@ def home(request):
     x = [1, 2, 3, 4, 5]
     y1 = [10, 20, 30, 40, 50]
     fig = make_subplots(rows=1, cols=1)
-    '''fig.update_layout(
-        width=auto,  # define a largura do gráfico em pixels
-        height=auto,  # define a altura do gráfico em pixels
-        margin=dict(l=20, r=20, t=30, b=20)  # define a margem em pixels para o gráfico
-    )'''
+    fig.update_layout(
+        width=400,
+        height=200,
+        margin=dict(l=20, r=20, t=20, b=20),
+        paper_bgcolor='rgb(255, 255, 255)',
+        plot_bgcolor='rgb(229, 229, 229)',
+    )
     fig.add_trace(go.Scatter(x=x, y=y1, name='Linha 1'), row=1, col=1)
     plot_div = fig.to_html(full_html=False, config={"displayModeBar": False, "scrollZoom": False})
     
@@ -30,8 +32,42 @@ def home(request):
     selected_item = request.GET.get('item')
     if len(datasets) == 0:
         context['choose'] = URL
-    if not selected_item:
+    elif not selected_item:
         selected_item = datasets[0]['name']
     context['choose'] = URL+"admin/"+selected_item
     context['dt_complete'] = [dt for dt in datasets if dt['name'] == selected_item][0]
     return HttpResponse(home.render(context, request))
+
+from django.views.decorators.csrf import csrf_exempt
+import json
+@csrf_exempt
+def graficos(request):
+    dados = request.body
+    dados = json.loads(dados)
+
+    batch = []
+    acc_x = []
+    hammings_loss = []
+    inv_hammings_loss = []
+    trusting = []
+
+    for i in dados:
+        acc_x.append(i['accuracy']*100)
+        hammings_loss.append(i['hamming_loss'])
+        inv_hammings_loss.append((1-i['hamming_loss'])*100)
+        trusting.append(i['trusting'])
+        batch.append(i['batch_id']) 
+
+    fig = make_subplots(rows=1, cols=1)
+    fig.update_layout(
+        width=400,
+        height=200,
+        margin=dict(l=20, r=20, t=20, b=20),
+        paper_bgcolor='rgb(255, 255, 255)',
+        plot_bgcolor='rgb(229, 229, 229)',
+    )
+    fig.update_xaxes(range = [0,len(batch)-1])
+    fig.update_yaxes(range = [0,100])
+    fig.add_trace(go.Scatter(x=batch, y=inv_hammings_loss, name='Concordância'), row=1, col=1)
+    plot_div_inv_hamming_loss = fig.to_html(full_html=False, config={"displayModeBar": False, "scrollZoom": False})
+    return JsonResponse({'graph_inv_hamming_loss': plot_div_inv_hamming_loss})
